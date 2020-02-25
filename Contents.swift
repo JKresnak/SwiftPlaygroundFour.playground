a8710a3e56cbf02e7382e4b6d9936208a346da52
Jkresnak <Jkresnak@hawaii.edu>
2020-02-25 10:23:00 -1000

    Initial commit

---
+++ Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+
+var str = "Hello, playground"
+
+
+//Starting the loop
+for number in 1...5
+{
+ //Printing the loop to make sure it works
+    print(number)
+}
+
+//Starting increments by defining sum
+var sum = 0
+//Starting increments by creating loop
+for number in 1...5
+{
+  // Number increment creation
+    sum += number
+   // Printing increments
+    print(sum)
+}
+
+//part 6
+var I = 1
+//Making increments with a while statement
+while I <= 10
+{
+   // printing while statement increments
+    print(I)
+   //Adding 1 to each number in the loop
+    I = I + 1
+}
